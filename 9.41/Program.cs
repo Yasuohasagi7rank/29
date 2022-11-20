08ff1e043762d90520d665c29a994ca53b824006
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:20:11 +0300

    Add files via upload

---
+++ 9.41/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9._41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "spartak";
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
